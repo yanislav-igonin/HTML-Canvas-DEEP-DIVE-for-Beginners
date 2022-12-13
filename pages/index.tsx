ae0c3b22ea2ce98e4044d96cc72bb3a8fc459221
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-13 13:08:31 +0200

    move animation out of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,15 @@
 import Canvas from '@components/Canvas';
+import { pulse } from 'animation';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = '#000000';
-    ctx.beginPath();
-    const x = ctx.canvas.width / 2;
-    const y = ctx.canvas.height / 2;
-    const radius = 200 * Math.sin(frameCount * 0.01) ** 2;
-    const endAngle = 2 * Math.PI;
-    ctx.arc(x, y, radius, 0, endAngle);
-    ctx.fill();
-  };
-
   return <div>
     <Head>
       <title>HTML Canvas DEEP DIVE for Beginners</title>
     </Head>
 
-    <Canvas draw={draw}/>
+    <Canvas draw={pulse}/>
   </div>;
 };
 
