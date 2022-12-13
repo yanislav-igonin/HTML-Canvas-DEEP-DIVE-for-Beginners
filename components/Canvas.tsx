49ee2056c806c31f58b84a09c95905564c583a54
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-13 13:06:07 +0200

    move animation out of canvas

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,21 +1,11 @@
 import { useRef, useEffect, type DetailedHTMLProps, type CanvasHTMLAttributes } from 'react';
 
 type Props = DetailedHTMLProps<CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
+ & { draw: (ctx: CanvasRenderingContext2D, frameCount: number) => void };
 
 const Canvas = (props: Props) => {
+  const { draw } = props;
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  
-  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = '#000000';
-    ctx.beginPath();
-    const x = ctx.canvas.width / 2;
-    const y = ctx.canvas.height / 2;
-    const radius = 200 * Math.sin(frameCount * 0.01) ** 2;
-    const endAngle = 2 * Math.PI;
-    ctx.arc(x, y, radius, 0, endAngle);
-    ctx.fill();
-  };
   
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement;
