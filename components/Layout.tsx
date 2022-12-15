0ed2f66cdeccf9970ded3d5e8771bda0711b32b2
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-15 17:44:14 +0200

    title now in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import { TopBar } from './TopBar';
 
 type Props = { children: React.ReactNode };
 
 export const Layout = ({ children }: Props) => {
   return <div>
+    <Head>
+      <title>HTML Canvas DEEP DIVE for Beginners</title>
+    </Head>
+    
     <TopBar />
     {children}
   </div>;
