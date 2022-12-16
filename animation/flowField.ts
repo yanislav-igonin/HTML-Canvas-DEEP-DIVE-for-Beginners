fd57ee1298cde4496df769c4c9eeae8a3bd3cd92
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-16 20:27:15 +0200

    uodate effect

--- animation/flowField.ts
+++ animation/flowField.ts
@@ -1,17 +1,26 @@
 let x = 0;
 let y = 0;
 export const flowField = (ctx: CanvasRenderingContext2D, frameCount: number) => {
-  const length = 10;
+  const length = 100;
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  x = centerX + Math.sin(frameCount) * 100;
+  y = centerY + Math.cos(frameCount) * 100;
   ctx.beginPath();
   ctx.moveTo(x, y);
-  x += length;
-  y += length;
-  ctx.lineTo(x * 0.2, y * 0.6);
+  ctx.lineTo(x + length, y + length);
   ctx.stroke();
 
-  if (x > ctx.canvas.width || y > ctx.canvas.height) {
-    x = 0;
-    y = 0;
+  // if (x > ctx.canvas.width || y > ctx.canvas.height) {
+  //   x = 0;
+  //   y = 0;
+  //   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // }
+
+  if (frameCount % 100 === 0) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   }
 };
