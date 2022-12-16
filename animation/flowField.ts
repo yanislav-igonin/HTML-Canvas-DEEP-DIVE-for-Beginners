9a45d5e5cbe8c069bdd103f0fcff996d5fb89ff4
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-16 22:54:11 +0200

    update flow field animation

--- animation/flowField.ts
+++ animation/flowField.ts
@@ -1,7 +1,30 @@
-export const flowField = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+type Point = {
+  x: number;
+  y: number;
+};
 
+const cellSize = 15;
+let length = 5;
+
+const drawLine = (ctx: CanvasRenderingContext2D, from: Point, to: Point) => {
+  ctx.beginPath();
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+};
+
+export const flowField = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+  for (let x = 0; x < ctx.canvas.width; x += cellSize) {
+    for (let y = 0; y < ctx.canvas.height; y += cellSize) {
+      const toX = x + length;
+      const toY = y + length;
+      drawLine(ctx, { x, y }, { x: toX, y: toY });
+    }
+  }
 };
 
 export const mouseMoveFlowField = (e: MouseEvent) => {
-  
+  // const { clientX, clientY } = e;
+  // const angle = Math.atan2(clientY, clientX);
+  // length = Math.sin(clientX) * 100 + Math.cos(clientY) * 100;
 };
