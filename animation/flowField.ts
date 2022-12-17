189b0b1e636ad1eeba9cf11ad6ebf2d14e593e75
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-17 14:52:17 +0200

    update flow field effect

--- animation/flowField.ts
+++ animation/flowField.ts
@@ -3,14 +3,24 @@ type Point = {
   y: number;
 };
 
-const cellSize = 15;
-let length = 5;
+const pointMouse: Point = {
+  x: 0,
+  y: 0,
+};
 
-const drawLine = (ctx: CanvasRenderingContext2D, from: Point, to: Point) => {
-  ctx.beginPath();
-  ctx.moveTo(from.x, from.y);
-  ctx.lineTo(to.x, to.y);
-  ctx.stroke();
+const cellSize = 10;
+let vr = 0.1;
+let radius = 5;
+const radiusThreshold = {
+  min: -20,
+  max: 20,
+};
+
+const getDistance = (p1: Point, p2: Point) => {
+  const dx = p1.x - p2.x;
+  const dy = p1.y - p2.y;
+  return dx ^ 2 + dy ^ 2;
+  // return Math.sqrt(dx ^ 2 + dy ^ 2);
 };
 
 const createGradient = (ctx: CanvasRenderingContext2D) => {
@@ -24,19 +34,38 @@ const createGradient = (ctx: CanvasRenderingContext2D) => {
   ctx.strokeStyle = gradient;
 };
 
+const drawLine = (ctx: CanvasRenderingContext2D, angle: number, from: Point, to: Point) => {
+  ctx.beginPath();
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+};
+
 export const flowField = (ctx: CanvasRenderingContext2D, frameCount: number) => {
-  for (let x = 0; x < ctx.canvas.width; x += cellSize) {
-    for (let y = 0; y < ctx.canvas.height; y += cellSize) {
-      const toX = x + length;
-      const toY = y + length;
-      drawLine(ctx, { x, y }, { x: toX, y: toY });
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  radius += vr;
+
+  if (radius > radiusThreshold.max || radius < radiusThreshold.min) {
+    vr *= -1;
+  }
+
+  for (let y = 0; y < ctx.canvas.height; y += cellSize) {
+    for (let x = 0; x < ctx.canvas.width; x += cellSize) {
+      const pointFrom = { x, y };
+      const distance = getDistance(pointFrom, pointMouse);
+      const length = distance / 5;
+      const angle = (Math.cos(x * 0.005) + Math.sin(y * 0.005)) * radius;
+      const toX = x + Math.cos(angle) * length;
+      const toY = y + Math.sin(angle) * length;
+      const pointTo = { x: toX, y: toY };
+      drawLine(ctx, angle, pointFrom, pointTo);
       createGradient(ctx);
     }
   }
 };
 
 export const mouseMoveFlowField = (e: MouseEvent) => {
-  // const { clientX, clientY } = e;
-  // const angle = Math.atan2(clientY, clientX);
-  // length = Math.sin(clientX) * 100 + Math.cos(clientY) * 100;
+  const { clientX, clientY } = e;
+  pointMouse.x = clientX;
+  pointMouse.y = clientY;
 };
