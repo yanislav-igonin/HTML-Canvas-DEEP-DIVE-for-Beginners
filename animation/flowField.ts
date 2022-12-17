82b08d73eabc47d772bca3aba2e66fb14f5970eb
Yanislav Igonin <yanislavdabest@gmail.com>
2022-12-17 12:55:06 +0200

    add gradient

--- animation/flowField.ts
+++ animation/flowField.ts
@@ -13,12 +13,21 @@ const drawLine = (ctx: CanvasRenderingContext2D, from: Point, to: Point) => {
   ctx.stroke();
 };
 
+const createGradient = (ctx: CanvasRenderingContext2D) => {
+  const { width, height } = ctx.canvas;
+  const gradient = ctx.createLinearGradient(0, 0, width, height);
+  gradient.addColorStop(0.1, 'red');
+  gradient.addColorStop(0.9, 'blue');
+  ctx.strokeStyle = gradient;
+};
+
 export const flowField = (ctx: CanvasRenderingContext2D, frameCount: number) => {
   for (let x = 0; x < ctx.canvas.width; x += cellSize) {
     for (let y = 0; y < ctx.canvas.height; y += cellSize) {
       const toX = x + length;
       const toY = y + length;
       drawLine(ctx, { x, y }, { x: toX, y: toY });
+      createGradient(ctx);
     }
   }
 };
